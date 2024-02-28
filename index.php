991cbc43d9655889d368b33d93ed61c4edf0c786
Your Name <feliss2003@outlook.com>
2024-02-28 11:07:35 -0500

    baseHtml

--- index.php
+++ index.php
@@ -9,12 +9,26 @@ require("./config.php");
   <title>Sell-it!</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="..//CSS/style.css" />
-  <link rel="stylesheet" href="..//CSS/normalize.css" />
-  <script src="..//JS/script.js"></script>
+  <link rel="stylesheet" href="/style.css" />
+  <link rel="stylesheet" href="/normalize.css" />
+  <script src="/script.js"></script>
 </head>
 
-<body>
+<body class="grid">
+  <header>
+    <img class="logo" src="/IMG/logo.png" alt="Logo">
+    <h1 class="titre">Sell-it!</h1>
+    <div>
+      <a href=""><img src="/IMG/profil.png" alt=""></a>
+      <a href=""><img src="/IMG/notifications.png" alt=""></a>
+      <a href=""><img src="/IMG/cart.png" alt=""></a>
+      <a href=""><img src="/IMG/messages.png" alt=""></a>
+    </div>
+  </header>
+
+  <nav>NAVBAR</nav>
+
+  <main>MAIN</main>
 </body>
 
 </html>
