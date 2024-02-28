67e1952e788a8484a8695e7d0191260d19283f3e
marmittos <feliss2003@outlook.com>
2024-02-27 23:41:34 -0500

    PretAStart

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ require("./config.php");
 <html lang="en">
 
 <head>
+  <title>Sell-it!</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="/CSS/style.css" />
-  <link rel="stylesheet" href="/CSS/normalize.css" />
-  <title>Sell-it!</title>
+  <link rel="stylesheet" href="..//CSS/style.css" />
+  <link rel="stylesheet" href="..//CSS/normalize.css" />
+  <script src="..//JS/script.js"></script>
 </head>
 
 <body>
-  <script src="/JS/javascript.js"></script>
 </body>
 
 </html>
