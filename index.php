b05f64f06840b8f1f0e93308534d76b5fd40ca48
marmittos <feliss2003@outlook.com>
2024-02-29 01:08:03 -0500

    Cleanup du Main, debut ajout articles

--- index.php
+++ index.php
@@ -15,11 +15,9 @@ require("./config.php");
 </head>
 
 <body class="grid">
-  <header>
+  <header class="headerInfos">
     <a href=""><img class="logo" src="/IMG/logo.png" alt="Logo" /></a>
-
-    <h1 class="titre">Sell-it!</h1>
-
+    <h1 class="title">Sell-it!</h1>
     <div class="icons">
       <a href=""><img src="/IMG/messages.png" alt="Messages" /></a>
       <a href=""><img src="/IMG/cart.png" alt="Panier" /></a>
@@ -28,50 +26,149 @@ require("./config.php");
     </div>
   </header>
 
-  <nav>
-    <form action="/search" method="GET" class="navbar">
-      <div>
-        <input type="text" class="textSearchbar" name="searchbar" placeholder="Rechercher..." />
-        <button type="submit" class="buttonSearchbar">Rechercher</button>
-      </div>
+  <form action="" method="GET" class="navbarOptions">
+    <div>
+      <label for="category">Catégorie :</label>
+      <select id="category" name="category" class="choiceCategory">
+        <option value="all">Toutes les catégories</option>
+        <option value="clothes">Vêtements</option>
+        <option value="shoes">Chaussures</option>
+        <option value="accessories">Accessoires</option>
+        <option value="electronics">Électronique</option>
+        <option value="books">Livres</option>
+        <option value="furniture">Meubles</option>
+        <option value="toys">Jouets</option>
+        <option value="vehicles">Véhicules</option>
+      </select>
+    </div>
 
-      <div>
-        <label for="category">Catégorie :</label>
-        <select id="category" name="category" class="choiceCategory">
-          <option value="all">Toutes les catégories</option>
-          <option value="clothes">Vêtements</option>
-          <option value="shoes">Chaussures</option>
-          <option value="accessories">Accessoires</option>
-          <option value="electronics">Électronique</option>
-          <option value="books">Livres</option>
-          <option value="furniture">Meubles</option>
-          <option value="toys">Jouets</option>
-          <option value="vehicles">Véhicules</option>
-        </select>
-      </div>
+    <div>
+      <label for="condition">État de l'objet :</label>
+      <select id="condition" name="condition" class="choiceCondition">
+        <option value="new">Neuf</option>
+        <option value="used">Occasion</option>
+        <option value="refurbished">Reconditionné</option>
+      </select>
+    </div>
+    <div>
+      <label for="maxPrice">Prix maximal :</label>
+      <input type="number" id="maxPrice" name="maxPrice" class="textMaxPrice" placeholder="Entrez le prix maximal" />
+    </div>
+    <div>
+      <label for="minPrice">Prix minimal :</label>
+      <input type="number" id="minPrice" name="minPrice" class="textMinPrice" placeholder="Entrez le prix minimal" />
+    </div>
+  </form>
 
-      <div>
-        <label for="condition">État de l'objet :</label>
-        <select id="condition" name="condition" class="choiceCondition">
-          <option value="new">Neuf</option>
-          <option value="used">Occasion</option>
-          <option value="refurbished">Reconditionné</option>
-        </select>
-      </div>
+  <form action="" method="GET" class="mainOptions">
+    <div>
+      <input type="text" class="textSearchbar" name="searchbar" placeholder="Rechercher..." />
+      <button type="submit" class="buttonSearchbar">Rechercher</button>
+    </div>
+    <div>
+      <label for="tab" class="labelChoiceTab">Onglet :</label>
+      <select id="tab" name="tab" class="choiceTab">
+        <option value="public">Publiques</option>
+        <option value="following">Abonnements</option>
+        <option value="self">Vos Publications</option>
+      </select>
+    </div>
+  </form>
 
-      <div>
-        <label for="maxPrice">Prix maximal :</label>
-        <input type="number" id="maxPrice" name="maxPrice" class="textMaxPrice" placeholder="Entrez le prix maximal" />
-      </div>
-
-      <div>
-        <label for="minPrice">Prix minimal :</label>
-        <input type="number" id="minPrice" name="minPrice" class="textMinPrice" placeholder="Entrez le prix minimal" />
-      </div>
-    </form>
-  </nav>
-
-  <main></main>
+  <main>
+    <div class="listingsContainer">
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+      <article>
+        <div class="listing">
+          <img src="/IMG/McQueen.jpg" alt="Placeholder" />
+          <h2 class="listingTitle">Titre de l'annonce</h2>
+          <p class="listingPrice">Prix : 0€</p>
+      </article>
+    </div>
+  </main>
 </body>
 
 </html>
